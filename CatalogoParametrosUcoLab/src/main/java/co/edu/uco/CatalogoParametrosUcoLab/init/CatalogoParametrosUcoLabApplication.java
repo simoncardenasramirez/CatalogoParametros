05da29fc341eb578d43782560857107f55f13fc1
simoncardenasramirez <simoncardenasramirez41@gmail.com>
2026-05-19 19:42:29 -0500

    refactorizacion de la ubicacion de el archivo de arranque

--- CatalogoParametrosUcoLab/src/main/java/co/edu/uco/CatalogoParametrosUcoLab/init/CatalogoParametrosUcoLabApplication.java
+++ CatalogoParametrosUcoLab/src/main/java/co/edu/uco/CatalogoParametrosUcoLab/init/CatalogoParametrosUcoLabApplication.java
@@ -1,4 +1,4 @@
-package co.edu.uco.CatalogoParametrosUcoLab;
+package co.edu.uco.CatalogoParametrosUcoLab.init;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
