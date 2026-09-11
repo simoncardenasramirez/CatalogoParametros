b0432015d65be09c3380d989d5bea7c7d620bbc0
simoncardenasramirez <simoncardenasramirez41@gmail.com>
2026-09-10 21:47:01 -0500

    ajuste sonar

--- CatalogoParametrosUcoLab/src/test/java/co/edu/uco/CatalogoParametrosUcoLab/CatalogoParametrosUcoLabApplicationTests.java
+++ CatalogoParametrosUcoLab/src/test/java/co/edu/uco/CatalogoParametrosUcoLab/CatalogoParametrosUcoLabApplicationTests.java
@@ -2,14 +2,29 @@ package co.edu.uco.CatalogoParametrosUcoLab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.azure.security.keyvault.secrets.SecretClient;
+import co.edu.uco.CatalogoParametrosUcoLab.infraestructure.secondaryadapters.surrealdb.SurrealDbClient;
+
+import static org.mockito.Mockito.verifyNoInteractions;
 
 import co.edu.uco.CatalogoParametrosUcoLab.init.CatalogoParametrosUcoLabApplication;
 
 @SpringBootTest(classes = CatalogoParametrosUcoLabApplication.class)
+@ActiveProfiles("test")
 class CatalogoParametrosUcoLabApplicationTests {
 
+    @MockitoBean
+    private SecretClient secretClient;
+
+    @MockitoBean
+    private SurrealDbClient surrealDbClient;
+
     @Test
-    void contextLoads() {
+    void debeCargarContextoCuandoNoHayServiciosExternos() {
+        verifyNoInteractions(secretClient, surrealDbClient);
     }
 
 }
